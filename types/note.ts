8a378ef045731290767f6ceef27a6a2c5010ab14
Sergii-Sotnikov <sotnikovss90@gmail.com>
2025-08-23 16:48:30 +0200

    Initial commit

---
+++ types/note.ts
@@ -0,0 +1,17 @@
+export interface NewNote {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  tag?: NoteTag;
+}
+
+export type NoteTag = "Todo"|"Work"|"Personal"|"Meeting"|"Shopping";
+
